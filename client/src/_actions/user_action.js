c9ab70b33119fa885ef8b288533a530889269bfd
ad <nogo22@naver.com>
2021-01-29 05:14:09 +0900

    add register func

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -9,6 +9,18 @@ export function loginUser(dataToSubmit) {
   //reducer로 보낸다
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post("/api/users/register", dataToSubmit)
+    .then((response) => response.data); //서버로 보냄
+
+  //reducer로 보낸다
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
